e472a140e9c0a71a20d2737d2ad2090a7ed3c5ab
Rohit Hazra <rohithzr@live.com>
2018-06-24 18:26:06 +0530

    adding travis

---
+++ push.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+#setup_git() {
+#  git config --global user.email "travis@travis-ci.org"
+#  git config --global user.name "Travis CI"
+#}
+#
+#}
+#commit_website_files() {
+#  git checkout -b gh-pages
+#  git add . *.html
+#  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+#}
+#
+#upload_files() {
+#  git remote add gh-pa git@github.com:zivost/zivost-com.git
+#  git remote add origin-pages https://${github_token}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+#  git push --quiet --set-upstream origin-pages gh-pages
+#}
+#
+#setup_git
+#commit_website_files
+#upload_files
